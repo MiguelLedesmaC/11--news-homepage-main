1d3e5134d462c6d6781deb58b1f9a285cd68a131
Miguel Ledesma <103437837+Xcorpi0@users.noreply.github.com>
2023-02-23 19:29:52 +0100

    modifico estilos

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+
 let hamburguer = document.querySelector('#hamburguer');
+let menu = document.querySelector('.nav-mobile')
+let modalBackground = document.querySelector('.modal-background')
 let isOpen = false;
 
+console.log(hamburguer);
 hamburguer.addEventListener('click',()=> {
     if(!isOpen){
-        
+        menu.style.display = 'flex';
+        modalBackground.style.visibility ='visible'
+        hamburguer.lastChild.src ='./assets/images/icon-menu-close.svg';
+        isOpen = true;
+    }else {
+        menu.style.display = 'none'
+        modalBackground.style.visibility ='hidden';
+        hamburguer.lastChild.src ='./assets/images/icon-menu.svg'
+        isOpen = false;
     }
 })
